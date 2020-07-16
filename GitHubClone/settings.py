d81d31bb5faa7dd1084fbd50fbf27d4cae991544
Boyuan Liu <boyuanliu6@yahoo.com>
2020-07-16 10:38:44 -0700

    added additional information to change

--- GitHubClone/settings.py
+++ GitHubClone/settings.py
@@ -180,4 +180,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+django_heroku.settings(config=locals(), staticfiles=False,logging=False)
