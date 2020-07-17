01341e606a6f5be8d83fc7b46beb8e5511d25998
Boyuan Liu <boyuanliu6@yahoo.com>
2020-07-16 17:45:45 -0700

    added breadcrumb and view file within web app

--- repo/views.py
+++ repo/views.py
@@ -33,17 +33,32 @@ def repo(request, username, repo, url="/"):
     if r.status == 1 and r.user_id != request.user.id:
         return HttpResponse("403")
 
+    if url.count("/") < 2 and url != "/":
+        url = url.split("/")[0]
+        directory_id = Directory.objects.get(repo_id=r.id, name="/").id
+        file = File.objects.get(directory_id=directory_id, filename=url)
+        r = requests.get(file.url)
+        return render(request, "repo/file.html", {
+            "content": r.text,
+            "repo": repo,
+            "username": username
+        })
+
     directory_id = Directory.objects.get(repo_id=r.id, name=url).id
     files = File.objects.filter(directory_id=directory_id)
 
     print(files)
 
     if url == "upload/":
-        return render(request, "repo/upload.html")
+        return render(request, "repo/upload.html", {
+            "repo": repo,
+            "username": username
+        })
 
     return render(request, "repo/repo.html", {
         "files": files,
-        "repo": repo
+        "repo": repo,
+        "username": username
     })
 
 
@@ -53,7 +68,10 @@ def upload(request, username, repo, url="/"):
         request.GET["url"]
     except Exception as e:
         print(e)
-        return render(request, "repo/upload.html")
+        return render(request, "repo/upload.html", {
+            "repo": repo,
+            "username": username
+        })
     print(colored(request.user.id, "yellow"))
     r = Repository.objects.get(user_id=request.user.id, name=repo).id
     print(r)
@@ -61,4 +79,7 @@ def upload(request, username, repo, url="/"):
     print(d)
     f = File(repo_id=r, filename=request.GET["filename"], directory_id=d, url=request.GET["url"])
     f.save()
-    return render(request, "repo/uploaded.html")
+    return render(request, "repo/uploaded.html", {
+        "repo": repo,
+        "username": username
+    })
