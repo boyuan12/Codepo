56edc52c5bac2b9a6f8a67c4a5bda26d0b210483
Boyuan Liu <boyuanliu6@yahoo.com>
2020-07-23 16:47:58 -0700

    added endpoint for directory & file mod.

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("repo/", views.get_structure)
+    path("repo/", views.get_structure),
+    path("create-directory/", views.create_directory),
+    path("delete-directory/", views.delete_repository),
+    path("modify-file/", views.modify_file),
+    path("add-file/", views.add_file),
+    path("delete-file/", views.delete_file)
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from main.models import Repository, Directory, File
 from django.http import HttpResponse, JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+from main.models import Repository, Directory, File
 from django.contrib.auth.models import User
+
+from termcolor import colored
 
 # Create your views here.
 def get_structure(request):
@@ -24,3 +27,137 @@ def get_structure(request):
         data["files"].append([f.id, f.filename, f.url, f.directory_id])
 
     return JsonResponse(data=data, safe=False)
+
+
+@csrf_exempt
+def create_directory(request):
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        user = User.objects.get(username=username)
+
+        repo = request.POST["repo"]
+        repo = Repository.objects.get(name=repo)
+
+        directory = request.POST["directory"] # /hellollo/welcomeworld/
+
+        dir = "/"
+        subdir = "/"
+        name = ""
+        if directory.count("/") > 2:
+            s = directory.split("/")
+            for i in range(1, len(s)-2):
+                print(i)
+                dir += s[i]
+            dir += "/"
+            print(colored(dir, "green"))
+            subdir += s[len(s)-2] + "/"
+            name = s[len(s)-2]
+        else:
+            name = directory.replace("/", "")
+
+        print(colored((subdir, name), "red"))
+        subdir = Directory.objects.get(repo_id=repo.id, path=dir)
+        d = Directory(repo_id=repo.id, dir_id=subdir.id, path=directory, name=name)
+        d.save()
+
+        return JsonResponse({"message": "success"})
+
+
+@csrf_exempt
+def delete_repository(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        user = User.objects.get(username=username)
+
+        repo = request.POST["repo"]
+        repo = Repository.objects.get(name=repo, user_id=user.id)
+
+        path = request.POST["path"]
+
+        d = Directory.objects.get(repo_id=repo.id, path=path)
+        File.objects.filter(repo_id=repo.id, directory_id=d.id).delete()
+        d.delete()
+
+        return JsonResponse({"message": "success"})
+
+
+@csrf_exempt
+def modify_file(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        user = User.objects.get(username=username)
+
+        repo = request.POST["repo"]
+        repo = Repository.objects.get(name=repo, user_id=user.id)
+
+        path = request.POST["path"]
+        filename = request.POST["filename"]
+        url = request.POST["url"]
+
+        print(url)
+
+        if path[0] == ".":
+            path = path[1:len(path)]
+
+        if path[len(path)-1] != "/":
+            path += "/"
+        
+        print(path)
+        d = Directory.objects.get(repo_id=repo.id, path=path)
+        f = File.objects.filter(repo_id=repo.id, directory_id=d.id, filename=filename).update(url=url)
+
+        return JsonResponse({"message": "success"})
+
+
+@csrf_exempt
+def add_file(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        user = User.objects.get(username=username)
+
+        repo = request.POST["repo"]
+        repo = Repository.objects.get(name=repo, user_id=user.id)
+
+        path = request.POST["path"]
+        filename = request.POST["filename"]
+        url = request.POST["url"]
+
+        if path[len(path)-1] == "/" and path[len(path)-2] == "/":
+            path = path[0:len(path)-1]
+
+        d = Directory.objects.get(repo_id=repo.id, path=path)
+        f = File(repo_id=repo.id, filename=filename, directory_id=d.id, url=url)
+        f.save()
+
+        return JsonResponse({"message": "success"})
+
+
+@csrf_exempt
+def delete_file(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        user = User.objects.get(username=username)
+
+        repo = request.POST["repo"]
+        repo = Repository.objects.get(name=repo, user_id=user.id)
+
+        path = request.POST["path"]
+        filename = request.POST["filename"]
+
+        try:
+            d = Directory.objects.get(repo_id=repo.id, path=path)
+            f = File.objects.get(repo_id=repo.id, filename=filename, directory_id=d.id).delete()
+        except Exception as e:
+            print(colored(str(e), "red"))
+            pass
+
+        return JsonResponse({"message": "success"})
