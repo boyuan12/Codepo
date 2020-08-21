3bd680fa3d8b67e412ecb1ab642aa3da6c78217b
Boyuan Liu <boyuanliu6@yahoo.com>
2020-08-21 15:40:29 -0700

    change the urls in repo

--- repo/urls.py
+++ repo/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("visibility/<str:username>/<str:repo>/", views.change_repo_visibility),
+    path("delete-repo/<str:username>/<str:repo>/", views.delete_repo),
     path("<str:username>/<str:repo>/folder/", views.create_folder),
     path("<str:username>/<str:repo>/folder/<path:url>/", views.create_folder),
     path("<str:username>/<str:repo>/upload/", views.upload),
     path("<str:username>/<str:repo>/upload/<path:url>/", views.upload),
     path("<str:username>/<str:repo>/", views.repo),
     path("<str:username>/<str:repo>/<path:url>/", views.repo),
-    path("visibility/<str:username>/<str:repo>/", views.change_repo_visibility),
-    path("delete-repo/<str:username>/<str:repo>/", views.delete_repo),
 ]
