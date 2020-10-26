cd876706d5798e8443fcfe76f52c1308a04a4ca9
Boyuan Liu <boyuanliu6@yahoo.com>
2020-10-25 22:48:15 -0700

    added simple print ip technique via django

--- main/views.py
+++ main/views.py
@@ -61,6 +61,12 @@ def validate_url(url):
 
 # Create your views here.
 def index(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    print(ip)
     if request.user.is_authenticated:
         repos = Repository.objects.filter(user_id=request.user.id)
         return render(request, "main/index.html", {
