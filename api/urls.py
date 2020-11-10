5e304eb36f8a3bb5fb4804641f4e303499216872
Boyuan Liu <boyuanliu6@yahoo.com>
2020-11-10 04:44:59 +0000

    added feature as proposed in #6

--- api/urls.py
+++ api/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path("gen-commit/", views.gen_commit_id),
     path("file-data/", views.file_data),
     path("commit/", views.commit),
+    path("query-repo/", views.query_repo),
     path("<str:username>/", views.get_user_info),
 ]
